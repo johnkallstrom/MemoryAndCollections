aac3e2e17bec8349170d73573523f95b558108e4
John Källström <johnkaellstrom@gmail.com>
2024-03-18 20:07:22 +0100

    Parenthesis class

--- SkalProj_Datastrukturer_Minne/Parenthesis.cs
+++ SkalProj_Datastrukturer_Minne/Parenthesis.cs
@@ -3,19 +3,21 @@
 	internal class Parenthesis
 	{
         public char Symbol { get; set; }
-        public bool Opening { get; set; }
+        public bool IsOpening { get; private set; }
+        public bool IsClosing { get; private set; }
         public ParenthesisType Type { get; set; }
 
-        public Parenthesis(ParenthesisType type, bool opening, char symbol)
+        public Parenthesis(ParenthesisType type, char symbol, bool isOpening, bool isClosing)
         {
             Type = type;
-            Opening = opening;
             Symbol = symbol;
+            IsOpening = isOpening;
+            IsClosing = isClosing;
         }
 
 		public override string ToString()
 		{
-            return $"Type: {Type}, Opening: {Opening}, Symbol: {Symbol}";
+            return $"Type: {Type}, Symbol: {Symbol}, Opening: {IsOpening}, Closing: {IsClosing}";
 		}
 	}
 }
