1c021860f87650c42f2d50e6df21fc45ffe903c1
John Källström <johnkaellstrom@gmail.com>
2024-03-18 15:23:55 +0100

    Add parenthesis class

---
+++ SkalProj_Datastrukturer_Minne/Parenthesis.cs
@@ -0,0 +1,67 @@
+﻿namespace SkalProj_Datastrukturer_Minne
+{
+	internal class Parenthesis
+	{
+        public char Symbol { get; set; }
+        public bool Opening { get; set; }
+        public ParenthesisType Type { get; set; }
+
+        public Parenthesis(ParenthesisType type, bool opening, char symbol)
+        {
+            Type = type;
+            Opening = opening;
+            Symbol = symbol;
+        }
+
+		public override string ToString()
+		{
+            string result = string.Empty;
+
+            switch (Type)
+            {
+                case ParenthesisType.Bracket:
+                    if (Opening)
+                    {
+                        result = "(";
+                    }
+                    else
+                    {
+                        result = ")";
+                    }
+                    break;
+                case ParenthesisType.Square:
+                    if (Opening)
+                    {
+                        result = "[";
+                    }
+                    else
+                    {
+                        result = "]";
+                    }
+                    break;
+                case ParenthesisType.Curly:
+                    if (Opening)
+                    {
+                        result = "{";
+                    }
+                    else
+                    {
+                        result = "}";
+                    }
+                    break;
+                case ParenthesisType.Angle:
+                    if (Opening)
+                    {
+                        result = "<";
+                    }
+                    else
+                    {
+                        result = ">";
+                    }
+                    break;
+            }
+
+            return result;
+		}
+	}
+}
---
+++ SkalProj_Datastrukturer_Minne/ParenthesisType.cs
@@ -0,0 +1,10 @@
+﻿namespace SkalProj_Datastrukturer_Minne
+{
+	internal enum ParenthesisType
+	{
+		Bracket = 1,
+		Square = 2,
+		Curly = 3,
+		Angle = 4
+	}
+}
